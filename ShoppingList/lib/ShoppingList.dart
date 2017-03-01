4d49412776d3bcdd33ccd1a3ddda7d64349e241b
JeAmoTu <JeAmoTu@gmail.com>
2017-03-01 23:50:52 +0800

    Update Comment.

--- ShoppingList/lib/ShoppingList.dart
+++ ShoppingList/lib/ShoppingList.dart
@@ -3,6 +3,7 @@ import 'package:ShoppingList/Product.dart';
 import 'package:ShoppingList/ShoppingListItem.dart';
 
 class ShoppingList extends StatefulWidget {
+  // key不清楚干嘛的
   ShoppingList({Key key, this.products}) : super(key: key);
 
   final List<Product> products;
@@ -12,11 +13,13 @@ class ShoppingList extends StatefulWidget {
   // widget (with the same key), the framework will re-use the State object
   // instead of creating a new State object.
 
+  // 创建一个状态
   @override
   _ShoppingListState createState() => new _ShoppingListState();
 }
 
 class _ShoppingListState extends State<ShoppingList> {
+  // product集合来存储对象
   Set<Product> _shoppingCart = new Set<Product>();
 
   void _handleCartChanged(Product product, bool inCart) {
@@ -25,6 +28,7 @@ class _ShoppingListState extends State<ShoppingList> {
       // inside a setState call to trigger a rebuild. The framework then calls
       // build, below, which updates the visual appearance of the app.
 
+      // 当点击list里的item时 会触发这段代码 然后重建 更新显示
       if (inCart)
         _shoppingCart.add(product);
       else
@@ -35,19 +39,22 @@ class _ShoppingListState extends State<ShoppingList> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-            title: new Text('Shopping List'),
-        ),
-        body: new ListView(
-            padding: new EdgeInsets.symmetric(vertical: 8.0),
-            children: config.products.map((Product product) {
-              return new ShoppingListItem(
-                  product: product,
-                  inCart: _shoppingCart.contains(product),
-                  onCartChanged: _handleCartChanged,
-              );
-            }).toList(),
-        ),
+      appBar: new AppBar(
+        title: new Text('Shopping List'),
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        //map 取出
+        children: config.products.map((Product product) {
+          return new ShoppingListItem(
+            product: product,
+            // 判断对象是否在集合中
+            inCart: _shoppingCart.contains(product),
+            //点击事件
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
     );
   }
 }
